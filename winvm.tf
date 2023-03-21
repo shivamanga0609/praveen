d8fa01dcb457e894acdc704497e7c91718f2f892
shivamanga0609 <114624074+shivamanga0609@users.noreply.github.com>
2023-03-20 20:36:05 -0500

    Create winvm.tf

---
+++ winvm.tf
@@ -0,0 +1,43 @@
+
+# Create Network Security Group and rules
+resource "azurerm_network_security_group" "smbc_nsg" {
+  name                = "smbc-nsg"
+  resource_group_name = data.azurerm_resource_group.resource-group.name
+  location            = var.location
+
+  security_rule {
+    name                       = "RDP"
+    priority                   = 1000
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "web"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "allow_ssh_sg"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
